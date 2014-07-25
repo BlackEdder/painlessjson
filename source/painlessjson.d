f9f0ca8576db0036add95c10840562105ac790e5
BlackEdder <edwin@tkwsping.nl>
2014-07-25 13:32:04 +0100

    Support for classes

--- source/painlessjson.d
+++ source/painlessjson.d
@@ -4,20 +4,52 @@ import std.traits;
 
 version( unittest ) {
 	import std.stdio;
+
+	struct Point {
+		double x = 0;
+		double y = 1;
+		this( double x_, double y_ ) { x = x_; y = y_; }
+		string foo() { writeln( "I should not be called" ); return "Noooooo!"; }
+	}
+
+	class PointC {
+		double x = 0;
+		double y = 1;
+		this( double x_, double y_ ) { x = x_; y = y_; }
+		string foo() { writeln( "I should not be called" ); return "Noooooo!"; }
+	}
 }
 
 /// Template function that converts any object to JSON
-JSONValue toJSON( T )( T object ) if (__traits(compiles, (T t) {JSONValue(t);})) {
-	return JSONValue( object );
-}
+JSONValue toJSON( T )( T object ) {
+	static if (__traits(compiles, (T t) {JSONValue(t);})) {
+		return JSONValue( object );
+	} else static if ( isArray!T ) { // Range
+		JSONValue[] jsonRange;
+		foreach ( el ; object ) {
+			jsonRange ~= el.toJSON;
+		}
+		return JSONValue(jsonRange);
+	} else {
+		JSONValue[string] json;	
 
-/*JSONValue toJSON( T )( T[] object ) {
-	JSONValue[] jsonRange;
-	foreach ( el ; object ) {
-		jsonRange ~= el.toJSON;
+		// Getting all member variables (there is probably an easier way)
+		foreach (name; __traits(allMembers, T))
+		{
+			static if(
+					__traits(compiles, __traits(getMember, object, name).toJSON)
+					//&& __traits(compiles, __traits(getMember, object, name)) 
+					//  Skip Functions 
+					&& !__traits(compiles, FunctionTypeOf!(__traits(getMember, object, name) ))
+					) 
+			{ // Can we get a value? (filters out void * this)
+				json[name] = __traits(getMember, object, name).toJSON;
+			}
+		}	
+
+		return JSONValue( json );
 	}
-	return JSONValue(jsonRange);
-}*/
+}
 
 /// Converting common types
 unittest {
@@ -33,11 +65,25 @@ unittest {
 }
 
 /// User structs
+unittest {
+ 	Point p;
+	assert( toJSON( p ).toString == q{{"x":0,"y":1}} );
+}
 
 /// Array of structs
+unittest {
+ 	Point[] ps = [ Point(-1,1), Point(2,3) ];
+	assert( toJSON( ps ).toString == q{[{"x":-1,"y":1},{"x":2,"y":3}]} );
+}
+
 
 /// User class
-
+unittest {
+ 	PointC p = new PointC( 0, 1 );
+	assert( toJSON( p ).toString == q{{"x":0,"y":1}} );
+}
 /// Array of classes
 
 /// Associative array
+
+/// Overloaded toJSON
