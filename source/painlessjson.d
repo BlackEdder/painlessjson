9f3fbf96cd365b389e142d6c7d6424f4b49d1864
BlackEdder <edwin@tkwsping.nl>
2014-07-25 10:37:21 +0100

    Added test for Range to json

--- source/painlessjson.d
+++ source/painlessjson.d
@@ -1,15 +1,43 @@
 import std.json;
+import std.range;
+import std.traits;
+
+version( unittest ) {
+	import std.stdio;
+}
 
 /// Template function that converts any object to JSON
-JSONValue toJSON( T )( T object ) {
+JSONValue toJSON( T )( T object ) if (__traits(compiles, (T t) {JSONValue(t);})) {
 	return JSONValue( object );
 }
 
+/*JSONValue toJSON( T )( T[] object ) {
+	JSONValue[] jsonRange;
+	foreach ( el ; object ) {
+		jsonRange ~= el.toJSON;
+	}
+	return JSONValue(jsonRange);
+}*/
+
 /// Converting common types
 unittest {
-	assert( 5.toJSON == JSONValue( 5 ) );
+	assert( 5.toJSON!int == JSONValue( 5 ) );
 	assert( 4.toJSON != JSONValue( 5 ) );
 	assert( 5.4.toJSON == JSONValue( 5.4 ) );
 	assert( toJSON( "test" ) == JSONValue( "test" ) );
 }
 
+/// Converting InputRanges
+unittest {
+	assert( [1,2].toJSON.toString == "[1,2]" );
+}
+
+/// User structs
+
+/// Array of structs
+
+/// User class
+
+/// Array of classes
+
+/// Associative array
