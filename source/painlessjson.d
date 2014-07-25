a5a9d6ebd9358451f4ca51cacbf8149f1804a300
BlackEdder <edder@tkwsping.nl>
2014-07-25 08:24:01 +0100

    Added tests for basic types

--- source/painlessjson.d
+++ source/painlessjson.d
@@ -1,9 +1,15 @@
 import std.json;
 
+/// Template function that converts any object to JSON
 JSONValue toJSON( T )( T object ) {
 	return JSONValue( object );
 }
 
+/// Converting common types
 unittest {
-	assert( true );
+	assert( 5.toJSON == JSONValue( 5 ) );
+	assert( 4.toJSON != JSONValue( 5 ) );
+	assert( 5.4.toJSON == JSONValue( 5.4 ) );
+	assert( toJSON( "test" ) == JSONValue( "test" ) );
 }
+
