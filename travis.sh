e992cbd2fc9220189e88989078e9302cf43cb2d8
BlackEdder <edder@tkwsping.nl>
2015-02-02 07:17:23 +0000

    Add script to generate documentation

---
+++ travis.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+dub test --compiler=${DC}
+
+if [ ! -z "$GH_TOKEN" ]; then
+    dub build -b docs --compiler=${DC}
+    cd docs
+    git init
+    git config user.name "Travis-CI"
+    git config user.email "travis@nodemeatspace.com"
+    git add .
+    git commit -m "Deployed to Github Pages"
+    git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}" docs:gh-pages > /dev/null 2>&1
+fi
