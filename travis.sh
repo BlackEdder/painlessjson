7d4f4977a5c45d35c9252032e6d480e78af6c9f7
BlackEdder <edder@tkwsping.nl>
2015-02-02 08:03:31 +0000

    Push from head to gh-pages

--- travis.sh
+++ travis.sh
@@ -13,5 +13,5 @@ if [ ! -z "$GH_TOKEN" ]; then
     git add .
     git commit -m "Deployed to Github Pages"
     #git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}" docs:gh-pages > /dev/null 2>&1
-    git push --force "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}" docs:gh-pages
+    git push --force "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}" HEAD:gh-pages
 fi
